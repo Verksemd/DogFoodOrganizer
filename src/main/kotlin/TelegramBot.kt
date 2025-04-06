043ea056c985501d9a4807a15da5dec8a7aec488
daria.smirnova <daria.smirnova@jetbrains.com>
2025-04-06 14:30:16 +0200

    added a new branch with segregated bot logic

--- src/main/kotlin/TelegramBot.kt
+++ src/main/kotlin/TelegramBot.kt
@@ -1,34 +1,24 @@
 import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.Dispatcher
 import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.handlers.HandleCommand
 import com.github.kotlintelegrambot.entities.ChatId
-
 
 class TelegramBot(
     private val notifier: Notifier,
     private var token: String
 ) {
+    private lateinit var dispatcher: Dispatcher
+    fun addHandler(command: String, handler: HandleCommand) {
+        dispatcher.command(command, handler)
+    }
+
     val bot: Bot = bot {
         token = this@TelegramBot.token
         dispatch {
-
-            command("start") {
-                val chatId = message.chat.id
-                send(chatId, "Привет! Я бот-кормилец 🐶. Доступные команды:\n" +
-                        "/checkenddate — когда закончится корм\n" +
-                "/checktoday - есть ли у собаки корм на сегодня")
-            }
-
-            command("checkenddate") {
-                val chatId = message.chat.id
-                notifier.informAboutFinishDate(chatId)
-            }
-
-            command("checktoday") {
-                val chatId = message.chat.id
-                notifier.checkToday(chatId)
-          }
+            dispatcher = this
         }
     }
 
